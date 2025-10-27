2200c724f9936b86126e408aa5d9678c4ee02c6e
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-27 09:43:15 -0300

    changed console colour

---
+++ GameProgGitBrnch&MrgCnflctsChrisGabeMatthew/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgGitBrnch_MrgCnflctsChrisGabeMatthew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Magenta;
+            Console.WriteLine("Hi-ya Ya round mudball!!!");
+            Console.ReadKey(true);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
