288b35185d634b001337ebc8c7b33789a84e714d
Terloaf <greenyryan2501@gmail.com>
2025-10-27 09:54:33 -0300

    Added new lines and changes text

---
+++ GameProgGitBrnch&MrgCnflctsChrisGabeMatthew/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgGitBrnch_MrgCnflctsChrisGabeMatthew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Hi-ya Ya round mudball!!!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hey-ya ya square blockhead!!!!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
